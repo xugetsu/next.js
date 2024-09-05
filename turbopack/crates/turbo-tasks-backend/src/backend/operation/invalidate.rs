a0e0b541afb3b9df0950a2d5b5617a8ecb0f5391
Tobias Koppers <tobias.koppers@googlemail.com>
2024-09-05 16:22:00 +0200

    WIP: logging

--- turbopack/crates/turbo-tasks-backend/src/backend/operation/invalidate.rs
+++ turbopack/crates/turbo-tasks-backend/src/backend/operation/invalidate.rs
@@ -61,10 +61,14 @@ impl Operation for InvalidateOperation {
 pub fn make_task_dirty(task_id: TaskId, queue: &mut AggregationUpdateQueue, ctx: &ExecuteContext) {
     let mut task = ctx.task(task_id);
 
+    println!("make_task_dirty {:?}", task_id);
+
     if task.add(CachedDataItem::Dirty { value: () }) {
         queue.extend(AggregationUpdateJob::data_update(
             &mut task,
             AggregatedDataUpdate::dirty_task(task_id),
         ));
+    } else {
+        println!("already dirty");
     }
 }
